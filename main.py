dda7e93b3c7cbd696f4687ef65c6576beb4e5d70
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-04-14 06:49:30 +0300

    Added get_blog_type method and path to demonstrate predefined values in a path using Enum.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'message':'Hello World!'}
+
+@app.get('/blog/all')
+def get_all_blogs():
+    return {'message': 'All blogs provided'}
+
+class BlogType(str, Enum):
+    short ='short'
+    story = 'story'
+    howto = 'howto'
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type:BlogType):
+    return {'message':f'Blog type {type}'}
+    
 
 @app.get('/blog/{id}')
 def get_blog(id: int):
