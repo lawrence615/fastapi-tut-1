3fa5f22e827c73094ae0ae9def84cf6d74701a70
Lawrence Macharia <macharialawrence2011@gmail.com>
2025-04-15 05:06:38 +0300

    The update shows how to define response status codes.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 from typing import Optional
 
@@ -34,6 +34,10 @@ def get_blog_type(type:BlogType):
     return {'message':f'Blog type {type}'}
     
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message':f'Blog with id {id}'}
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found.'}
+    else:
+        return {'message':f'Blog with id {id}'}
